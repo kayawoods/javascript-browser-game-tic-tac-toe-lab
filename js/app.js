7d2c810091429811cfc88fcf8bf1d39eef77d6d5
Kaya Woods <kayanevada@gmail.com>
2025-01-29 10:40:26 -0800

    second commit

--- js/app.js
+++ js/app.js
@@ -7,16 +7,16 @@ const winningCombos = [
     [6, 7, 8,],
     [0, 3, 6,],
     [1, 4, 7,],
-    [2, 5, 6,],
+    [2, 5, 8,],
     [0, 4, 8,],
     [2, 4, 6,],
 ]
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board = ['X', 'O', '', '', '', '', '', '', '',]
-let turn = 'X';
-let winner = true;
-let tie = true;
+let board;
+let turn;
+let winner;
+let tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -26,8 +26,10 @@ const resetBtnEl = document.querySelector('#reset')
 
 /*-------------------------------- Functions --------------------------------*/
 function init() {
-
-
+    board = ['', '', '', '', '', '', '', '', '',];
+    turn = 'X';
+    winner = false;
+    tie = false;
     render()
 };
 
@@ -47,79 +49,83 @@ function updateBoard() {
             squaredEls[index].innerText = 'X'
         } else if (square === 'O') {
             squaredEls[index].innerText = 'O'
+        } else if (square === '') {
+            squaredEls[index].innerText = ''
         }
-    })
+    }
+    )
 }
 
 function updateMessage() {
     if (winner === false && tie === false) {
-        messageEls.innerText = 'it is X turn'
+        messageEls.innerText = `${turn}`
     } else if (winner === false && tie === true) {
         messageEls.innerText = 'tie game'
     } else {
-        messageEls.innerText = 'you are the winner'
+        messageEls.innerText = `${turn} won!`
     }
 }
 
-function handleClick(event){
-  const squareIndex = event.target.id;
-  console.log(winner);
-  placePiece(squareIndex);
-  checkForWinner();
-  checkForTie();
-  switchPlayerTurn(); 
-  if (board[squareIndex] === 'X' || board[squareIndex] === 'O') {
-    return
-  }
-  if (winner === true){
-    return
-  } console.log(squareIndex);
-  
+function handleClick(event) {
+    const squareIndex = event.target.id;
+
+    if (board[squareIndex] === 'X' || board[squareIndex] === 'O') {
+        return
+    }
+    if (winner === true) {
+        return
+    }
+    placePiece(squareIndex);
+    checkForWinner(); 
+    checkForTie(); 
+    switchPlayerTurn(); 
+    render();
 }
 
 function placePiece(index) {
-    board[index] = turn 
+    board[index] = turn
     console.log(board)
-;    
+        ;
 }
 
 function checkForWinner() {
     if (
-        (board[0] !== '' && board[0] === board[1] && board[0] === board[2]) || 
-        (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) || 
-        (board[6] !== '' && board[6] === board[7] && board[6] === board[8]) || 
-        (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) || 
-        (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) || 
-        (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) || 
-        (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) || 
-        (board[2] !== '' && board[2] === board[4] && board[2] === board[6])    
+        (board[0] !== '' && board[0] === board[1] && board[0] === board[2]) ||
+        (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) ||
+        (board[6] !== '' && board[6] === board[7] && board[6] === board[8]) ||
+        (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) ||
+        (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) ||
+        (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) ||
+        (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) ||
+        (board[2] !== '' && board[2] === board[4] && board[2] === board[6])
     ) {
         winner = true;
-    
+
     }
+    console.log(winner)
 }
 
 
-function checkForTie () {
-    if (winner = true){ 
-        return 
-    } else if (board[index] = '')  { 
-tie = false 
-    } else { 
-        tie = true 
+function checkForTie() {
+    if (winner) {
+        return
+    } else if (board.includes('')) {
+        tie = false
+    } else {
+        tie = true
     }
 
 }
 function switchPlayerTurn() {
-    if (winner) {  
-        return; 
+    if (winner) {
+        return;
     }
-     if (turn === 'X') {
-        turn = '0'; 
-     } else {
-        turn = 'X'; 
-     }
-     };
+    if (turn === 'X') {
+        turn = 'O';
+    } else {
+        turn = 'X';
+    }
+};
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -127,14 +133,14 @@ squaredEls.forEach(square => {
     square.addEventListener('click', handleClick);
 });
 
-resetBtnEl.addEventListener('click', init); 
+resetBtnEl.addEventListener('click', init);
 
 init()
 
 
 
 
-
+// 
 
 
 
